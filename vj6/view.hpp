dd15b88440aa929820d421a489c73dfe73e7e8be
AntePera <92494174+AntePera@users.noreply.github.com>
2021-12-22 22:40:14 +0100

    Delete vj6 directory

--- vj6/view.hpp
+++
@@ -1,9 +0,0 @@
-#include"header.hpp"
-class HMview
-{
-public:
-	void displayAns(HMmodel mode);
-	void displayCurrentProgress(HMmodel mode);
-	void displayUsedLetters(HMmodel mode);
-	void displayHangman(HMmodel mode);
-};
